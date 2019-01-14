d138ce48877318aa0acd0009e11515794cb87bdc
jaechang <ljchang94@kaist.ac.kr>
2019-01-15 07:38:47 +0900

    sample.py is updated

--- sample.py
+++ sample.py
@@ -13,6 +13,10 @@ import argparse
 import torch.multiprocessing as mp
 from torch.multiprocessing import Pool
 
+def normalize(v, max_v, min_v):
+    v = min(max_v, v)
+    v = max(min_v, v)
+    return (v-min_v)/(max_v-min_v)
 
 #c1ccc(Nc2ncnc3c2oc2ccccc23)cc1 CHEMBL149512 Brc4cccc(Nc2ncnc3c1ccccc1oc23)c4 6.130768280269024
 
@@ -50,12 +54,21 @@ if __name__ == '__main__':
     parser.add_argument('--dim_of_edge_vector', help = 'dimension of edge vector', type = int, default = 128) 
     parser.add_argument('--dim_of_FC', help = 'dimension of FC', type = int, default = 128) 
     parser.add_argument('--save_fpath', help = 'file path of saved model', type = str) 
+    parser.add_argument('--scaffold', help = 'smiles of scaffold', type = str) 
+    parser.add_argument('--target_property', help = 'value of target property', type = float) 
+    parser.add_argument('--scaffold_property', help = 'valud of scaffold property', type = float) 
+    parser.add_argument('--output_filename', help = 'output file name', type = str) 
+    parser.add_argument('--minimum_value', help = 'minimum value of property. It will be used for nomralization', type = float) 
+    parser.add_argument('--maximum_value', help = 'maximum value of property. It will be used for nomralization', type = float) 
     args = parser.parse_args()
     
     #hyperparameters
     ncpus = args.ncpus
     item_per_cycle = args.item_per_cycle
     save_fpath = args.save_fpath
+    target_property=normalize(args.target_property, args.maximum_value, args.minimum_value)
+    scaffold_property=normalize(args.scaffold_property, args.maximum_value, args.minimum_value)
+
     #lines for multiprocessing
     mp.set_start_method('spawn')
     torch.manual_seed(1)
@@ -69,7 +82,7 @@ if __name__ == '__main__':
     #initialize parameters of the model 
     shared_model = initialize_model(shared_model, save_fpath)
     
-    scaffold = 'c1ccc(Nc2ncnc3c2oc2ccccc23)cc1' 
+    scaffold = args.scaffold
     scaffold = [scaffold for i in range(item_per_cycle)]
     smiles = [None for i in range(item_per_cycle)]
     retval = mp.Manager().list()
@@ -77,7 +90,9 @@ if __name__ == '__main__':
     st = time.time()
     processes = []
     for i in range(ncpus):
-        p = mp.Process(target=sample, args=(shared_model, smiles, scaffold,  np.array([[1,0]]), np.array([[0.85,0.15]]), i, retval, args))
+        condition1 = np.array([[target_property, 1-target_property]])
+        condition2 = np.array([[scaffold_property, 1-scaffold_property]])
+        p = mp.Process(target=sample, args=(shared_model, smiles, scaffold,  condition1, condition2, i, retval, args))
         p.start()
         processes.append(p)
         time.sleep(0.1)
@@ -89,7 +104,7 @@ if __name__ == '__main__':
     print ('before remove duplicate : ', len(valid))
     valid = list(set(valid))
     print ('after remove duplicate', len(valid))
-    w = open('generated_molecule.txt', 'w')
+    w = open(args.output_filename, 'w')
     w.write(scaffold[0]+'\toriginal\tegfr\n')
     for idx in range(len(valid)):
         w.write(valid[idx] + '\t' + 'gen_' + str(idx) + '\tegfr\n')
