63ef7852f2d62cfd7a954144ca63454210736120
jaechang <ljchang94@kaist.ac.kr>
2019-01-18 13:57:01 +0900

    keys for SA_score is added

---
+++ sas_keys/plot.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt
+with open('data.txt') as f:
+    lines = f.read().split('\n')
+    lines = [l.split('\t') for l in lines]
+    condition = [float(l[3]) for l in lines[:10000]]
+    plt.hist(condition)
+    plt.show()
