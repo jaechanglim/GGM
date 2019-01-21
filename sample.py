c7e91927c53eac9c6a2c8cec084bb0f36cfcd671
sangyeon-hwang <s.hwang@kaist.ac.kr>
2019-01-22 03:05:20 +0900

    Start a new branch `multi`
    KNOWN BUG: the current version only generates duplicative new molecules!
    
    The new branch is for enabling multi-property training of the original code.
    Major changes made are as follows:
        - ggm.ggm.__init__ requires an additional argument `N_conditions`.
        - Training-data processing functions are added:
            utils.load_data
            utils.sample_data
            utils.active_ratios
        - New (or changed) arguments of vaetrain.py:
            --key_dir -> --key_dirs
            --active_ratio
        - New (or changed) arguments of sample.py:
            --target_property -> --target_properties
            --scaffold_property -> --scaffold_properties
            --minimum_value -> --minimum_values
            --maximum_value -> --maximum_values
        - ggm.ggm.sample now accepts condition vectors of shape (N,), not (1,N).
        - "*/id_to_condition*.pkl" data structures were changed from
            { ID1:[c1, 1-c1], ID2:[c2, 1-c2], ... }
          to
            { ID1:c1, ID2:c2, ... }
        - Rename: "*/id_to_condition1.pkl" -> "*/id_to_whole_condition.pkl"
        - Rename: "*/id_to_condition2.pkl" -> "*/id_to_scaffold_condition.pkl"

--- sample.py
+++ sample.py
@@ -21,9 +21,7 @@ def normalize(v, max_v, min_v):
     v = max(min_v, v)
     return (v-min_v)/(max_v-min_v)
 
-#c1ccc(Nc2ncnc3c2oc2ccccc23)cc1 CHEMBL149512 Brc4cccc(Nc2ncnc3c1ccccc1oc23)c4 6.130768280269024
-
-def sample(shared_model, smiles, scaffold, condition1, condition2, pid, retval_list, args):
+def sample(shared_model, wholes, scaffolds, condition1, condition2, pid, retval_list, args):
     """\
     Target function for the multiprocessed sampling.
 
@@ -33,9 +31,9 @@ def sample(shared_model, smiles, scaffold, condition1, condition2, pid, retval_l
     ----------
     shared_model: torch.nn.Module
         A shared trained model to be used in the sampling.
-    smiles: list[str] | list[None]
+    wholes: list[str] | list[None]
         A list of whole SMILESs to be used as latent vectors.
-    scaffold: list[str]
+    scaffolds: list[str]
         A list of scaffold SMILESs from which new SMILESs will be sampled.
     condition1: list[float]
         A list of target property values.
@@ -52,13 +50,12 @@ def sample(shared_model, smiles, scaffold, condition1, condition2, pid, retval_l
         Delivers hyperparameters from command arguments to the model.
     """
     #optimizer = optim.Adam(shared_model.parameters(), lr=1e-4)
-    model=ggm(args)
+    model=ggm(args, len(condition1) + len(condition2))
     st1 = time.time()
 
-    for idx, (s1,s2) in enumerate(zip(smiles, scaffold)):
+    for idx, (s1,s2) in enumerate(zip(wholes, scaffolds)):
         model.load_state_dict(shared_model.state_dict())
-        retval = model.sample(None, s2, latent_vector=None, condition1 = condition1, condition2 = condition2, stochastic=args.stochastic)
-        #retval = model.sample(s1, s2, latent_vector=None, stochastic=False)
+        retval = model.sample(s1, s2, latent_vector=None, condition1=condition1, condition2=condition2, stochastic=args.stochastic)
         #retval = shared_model(s)
         if retval is None: continue
         g_gen, h_gen  = retval
@@ -70,7 +67,6 @@ def sample(shared_model, smiles, scaffold, condition1, condition2, pid, retval_l
         # Save the given whole SMILES and the new SMILES.
         retval_list[pid].append((s1, new_smiles))
     end1 = time.time()
-    return
     #print ('accumulate time', pid, end1-st1)
 
 if __name__ == '__main__':
@@ -83,12 +79,12 @@ if __name__ == '__main__':
     parser.add_argument('--dim_of_FC', help = 'dimension of FC', type = int, default = 128) 
     parser.add_argument('--save_fpath', help = 'file path of saved model', type = str) 
     parser.add_argument('--scaffold', help = 'smiles of scaffold', type = str) 
-    parser.add_argument('--target_property', help = 'value of target property', type = float) 
-    parser.add_argument('--scaffold_property', help = 'valud of scaffold property', type = float) 
+    parser.add_argument('--target_properties', help = 'values of target properties', nargs='+', type = float) 
+    parser.add_argument('--scaffold_properties', help = 'values of scaffold properties', nargs='+', type = float) 
     parser.add_argument('--output_filename', help = 'output file name', type = str) 
-    parser.add_argument('--minimum_value', help = 'minimum value of property. It will be used for normalization', type = float) 
-    parser.add_argument('--maximum_value', help = 'maximum value of property. It will be used for normalization', type = float) 
-    parser.add_argument('--stochastic', help = 'stocahstically add node and edge', dest='stochastic', action='store_true') 
+    parser.add_argument('--minimum_values', help = 'minimum values of properties. It will be used for normalization', nargs='+', type = float) 
+    parser.add_argument('--maximum_values', help = 'maximum values of properties. It will be used for normalization', nargs='+', type = float) 
+    parser.add_argument('--stochastic', help = 'stocahstically add node and edge', action='store_true') 
     args = parser.parse_args()
     
     #hyperparameters
@@ -98,15 +94,15 @@ if __name__ == '__main__':
     output_filename = os.path.expanduser(args.output_filename)
 
     # Normalize the property values to be in [0, 1].
-    target_property = normalize(args.target_property, args.maximum_value, args.minimum_value)
-    scaffold_property = normalize(args.scaffold_property, args.maximum_value, args.minimum_value)
+    target_properties = [normalize(*values) for values in zip(args.target_properties, args.maximum_values, args.minimum_values)]
+    scaffold_properties = [normalize(*values) for values in zip(args.scaffold_properties, args.maximum_values, args.minimum_values)]
 
     #lines for multiprocessing
     mp.set_start_method('spawn')
     torch.manual_seed(1)
 
     #model 
-    shared_model = ggm(args)
+    shared_model = ggm(args, len(target_properties) + len(scaffold_properties))
     shared_model.share_memory()
 
     print ("Model #Params: %dK" % (sum([x.nelement() for x in shared_model.parameters()]) / 1000,))
@@ -117,8 +113,8 @@ Num of generations: {item_per_cycle} per CPU (Total {ncpus*item_per_cycle})
 Model path        : {os.path.abspath(save_fpath)}
 Output path       : {os.path.abspath(output_filename)}
 Scaffold          : {args.scaffold}
-Scaffold property : {args.scaffold_property} -> {scaffold_property}
-Target property   : {args.target_property} -> {target_property}
+Scaffold values   : {args.scaffold_properties} -> {scaffold_properties}
+Target values     : {args.target_properties} -> {args.target_properties}
 dim_of_node_vector: {args.dim_of_node_vector}
 dim_of_edge_vector: {args.dim_of_edge_vector}
 dim_of_FC         : {args.dim_of_FC}
@@ -128,22 +124,21 @@ stochastic        : {args.stochastic}
     #initialize parameters of the model 
     shared_model = utils.initialize_model(shared_model, save_fpath)
     
-    scaffold = args.scaffold
     # Copy the same scaffold SMILES for multiple generations.
-    scaffold = [scaffold for i in range(item_per_cycle)]
+    scaffolds = [args.scaffold for i in range(item_per_cycle)]
     # A whole SMILES can be given and become a latent vector for decoding,
     # but here it is given as None so that a latent is randomly sampled.
-    smiles = [None for i in range(item_per_cycle)]
-    condition1 = np.array([[target_property, 1-target_property]])
-    condition2 = np.array([[scaffold_property, 1-scaffold_property]])
+    wholes = [None for i in range(item_per_cycle)]
+    condition1 = target_properties.copy()
+    condition2 = scaffold_properties.copy()
     
     # A list of multiprocessing.managers.ListProxy to collect SMILES
     retval_list = [mp.Manager().list() for i in range(ncpus)]
     st = time.time()
     processes = []
     
-    for i in range(ncpus):
-        p = mp.Process(target=sample, args=(shared_model, smiles, scaffold, condition1, condition2, i, retval_list, args))
+    for pid in range(ncpus):
+        p = mp.Process(target=sample, args=(shared_model, wholes, scaffolds, condition1, condition2, pid, retval_list, args))
         p.start()
         processes.append(p)
         time.sleep(0.1)
@@ -158,8 +153,7 @@ stochastic        : {args.stochastic}
     valid = list(set(valid))
     print ('after remove duplicate', len(valid))
     w = open(args.output_filename, 'w')
-    w.write(scaffold[0]+'\toriginal\tegfr\n')
+    w.write(scaffolds[0]+'\toriginal\tegfr\n')
     for idx in range(len(valid)):
         w.write(valid[idx] + '\t' + 'gen_' + str(idx) + '\n')
     w.close()                
-
--- vaetrain.py
+++ vaetrain.py
@@ -1,7 +1,6 @@
 import argparse
 from collections import OrderedDict
 import os
-import pickle
 import random
 import time
 
@@ -16,7 +15,7 @@ from ggm import ggm
 from shared_optim import SharedRMSprop, SharedAdam
 import utils
 
-def train(shared_model, optimizer, smiles, scaffold, condition1, condition2, pid, retval_list, args):
+def train(shared_model, optimizer, wholes, scaffolds, whole_conditions, scaffold_conditions, pid, retval_list, args):
     """\
     Target function for the multiprocessed training.
 
@@ -29,14 +28,17 @@ def train(shared_model, optimizer, smiles, scaffold, condition1, condition2, pid
         A shared model to be trained.
     optimizer: torch.optim.Optimizer
         A shared optimizer.
-    smiles: list[str]
-        A list of whole SMILESs.
-    scaffold: list[str]
-        A list of scaffold SMILESs corresponding to `smiles`.
-    condition1: list[float]
-        A list of whole property values for CVAE.
-    condition2: list[float]
-        A list of scaffold property values for CVAE.
+    wholes: list[str]
+        A list of whole-molecule SMILESs.
+    scaffolds: list[str]
+        A list of scaffold SMILESs.
+    whole_conditions: list[ list[float] ]
+        [
+            [ value1, value2, ... ],  # condition values of whole 1
+            [ value1, value2, ... ],  # condition values of whole 2
+        ]
+    scaffold_conditions: list[ list[float] ]
+        Similar to `whole_conditions`, but with scaffold values.
     pid: int
         CPU index.
     retval_list: list[multiprocessing.managers.ListProxy]
@@ -47,15 +49,16 @@ def train(shared_model, optimizer, smiles, scaffold, condition1, condition2, pid
         Delivers hyperparameters from command arguments to the model.
     """
     #each thread make new model
-    model=ggm(args)
-    for idx in range(len(smiles)):
+    # Number of conditions <- (number of properties) * 2
+    model=ggm(args, len(args.key_dirs)*2)
+    for idx in range(len(wholes)):
         #set parameters of model as same as that of reference model
         model.load_state_dict(shared_model.state_dict())
         model.zero_grad()
         optimizer.zero_grad()
         
         #forward
-        retval = model(smiles[idx], scaffold[idx], condition1[idx], condition2[idx], args.shuffle_order)
+        retval = model(wholes[idx], scaffolds[idx], whole_conditions[idx], scaffold_conditions[idx], args.shuffle_order)
         
         #if retval is None, some error occured. it is usually due to invalid smiles
         if retval is None:
@@ -84,11 +87,12 @@ if __name__ == '__main__':
     parser.add_argument('--save_every', help = 'choose how often model will be saved', type = int, default = 200) 
     parser.add_argument('--beta1', help = 'beta1: lambda paramter for VAE training', type = float, default = 5e-3) 
     parser.add_argument('--save_dir', help = 'save directory', type = str) 
-    parser.add_argument('--key_dir', help = 'key directory', type = str) 
-    parser.add_argument('--shuffle_order', help = 'shuffle order or adding node and edge', dest='shuffle_order', action='store_true') 
+    parser.add_argument('--key_dirs', help='key directories', nargs='+', metavar='PATH')
+    parser.add_argument('--shuffle_order', help = 'shuffle order or adding node and edge', action='store_true') 
+    parser.add_argument('--active_ratio', help='active ratio in sampling (default: no matter)', type=float)
     args = parser.parse_args()
     args.save_dir = os.path.expanduser(args.save_dir)
-    args.key_dir = os.path.expanduser(args.key_dir)
+    key_dirs = [os.path.expanduser(path) for path in args.key_dirs]
     if not os.path.isdir(args.save_dir):
         os.mkdir(args.save_dir)
 
@@ -104,7 +108,8 @@ if __name__ == '__main__':
     torch.manual_seed(1)
 
     #model 
-    shared_model = ggm(args)
+    # Number of conditions <- (number of properties) * 2
+    shared_model = ggm(args, len(args.key_dirs)*2)
     shared_model.share_memory()  # torch.nn.Module.share_memory
 
     #shared optimizer
@@ -115,29 +120,11 @@ if __name__ == '__main__':
     #initialize parameters of the model 
     shared_model = utils.initialize_model(shared_model, False)
 
-    #load data and keys
-    with open(args.key_dir+'/train_active_keys.pkl', 'rb') as f:
-        train_active_keys = pickle.load(f)
-        # list[str] -> ['STOCK4S-00033', 'STOCK4S-00102', ...]
-    with open(args.key_dir+'/train_inactive_keys.pkl', 'rb') as f:
-        train_inactive_keys = pickle.load(f)
-        # list[str] -> ['STOCK4S-00033', 'STOCK4S-00102', ...]
-    with open(args.key_dir+'/test_active_keys.pkl', 'rb') as f:
-        test_active_keys = pickle.load(f)
-        # list[str] -> ['STOCK4S-00033', 'STOCK4S-00102', ...]
-    with open(args.key_dir+'/test_inactive_keys.pkl', 'rb') as f:
-        test_inactive_keys = pickle.load(f)
-        # list[str] -> ['STOCK4S-00033', 'STOCK4S-00102', ...]
-    with open(args.key_dir+'/id_to_smiles.pkl', 'rb') as f:
-        id_to_smiles = pickle.load(f)
-        # dict[str, list[str]] -> {'STOCK4S-67369': [whole_SMILES, scaffold_SMILES], ...}
-    with open(args.key_dir+'/id_to_condition1.pkl', 'rb') as f:
-        id_to_condition1 = pickle.load(f)
-        # dict[str, list[float]] -> {'STOCK4S-67369': [whole_value, 1-whole_value], ...}
-    with open(args.key_dir+'/id_to_condition2.pkl', 'rb') as f:
-        id_to_condition2 = pickle.load(f)
-        # dict[str, list[float]] -> {'STOCK4S-67369': [scaffold_value, 1-scaffold_value], ...}
-        # The property values (affinity, TPSA, logP and MW) are all normalized within [0, 1].
+    # Load data and keys.
+    # See `utils.load_data` for the variable structures.
+    ( id_to_smiles,
+      id_to_whole_conditions, id_to_scaffold_conditions,
+      active_keys, inactive_keys ) = utils.load_data(key_dirs, 'train')
 
     num_cycles = int(len(id_to_smiles)/ncpus/item_per_cycle)
     print(f"""\
@@ -148,43 +135,51 @@ Number of epochs  : {num_epochs}
 Number of cycles  : {num_cycles} per epoch
 Minibatch size    : {item_per_cycle} per CPU per cycle
 Save model every  : {save_every} cycles per epoch (Total {num_epochs*(num_cycles//save_every+1)} models)
+Save directory    : {os.path.abspath(args.save_dir)}
 beta1             : {args.beta1}
 Learning rate     : {lr}
 dim_of_node_vector: {args.dim_of_node_vector}
 dim_of_edge_vector: {args.dim_of_edge_vector}
 dim_of_FC         : {args.dim_of_FC}
 shuffle_order     : {args.shuffle_order}
+Data directories  : {key_dirs}
 """)
     
-    print("# epoch  cycle_in_epoch  total_cycle  loss  loss1  loss2  loss3  time")
+    print("epoch  cyc  totcyc  loss  loss1  loss2  loss3  time")
     for epoch in range(num_epochs):
-        for c in range(num_cycles):
+        for cycle in range(num_cycles):
             retval_list = mp.Manager().list()  # Is this needed?
             # List of multiprocessing.managers.ListProxy to collect losses
             retval_list = [mp.Manager().list() for i in range(ncpus)]
             st = time.time()
             processes = []
-            for i in range(ncpus):
-                #sample active and inactive keys with same ratio
-                keys1 = [random.choice(train_active_keys) for i in range(int(item_per_cycle/2))]
-                keys2 = [random.choice(train_inactive_keys) for i in range(int(item_per_cycle/2))]
-                keys = keys1 + keys2
-                random.shuffle(keys)
+            for pid in range(ncpus):
+                # Sample keys without considering activeness.
+                if args.active_ratio is None:
+                    keys = random.sample(id_to_smiles.keys(), item_per_cycle)
+                # Sample active and inactive keys by the required ratio.
+                else:
+                    keys = utils.sample_data(acaive_keys, inactive_keys, item_per_cycle, args.active_ratio)
 
-                #activities work as condition. we need both activities of whole molecule and scaffold
-                condition1 = [id_to_condition1[k] for k in keys]  # [[whole_value, 1-whole_value], ...]
-                condition2 = [id_to_condition2[k] for k in keys]  # [[scaffold_value, 1-scaffold_value], ...]
+                # Property (descriptor) values work as conditions;
+                # we need both values of whole molecules and scaffolds.
+                # whole_conditions := [
+                #     [ value1, value2, ... ],  # condition values of whole 1
+                #     [ value1, value2, ... ],  # condition values of whole 2
+                #     ... ]
+                whole_conditions = [id_to_whole_conditions[key] for key in keys]
+                scaffold_conditions = [id_to_scaffold_conditions[key] for key in keys]
 
-                #we need smiles of whole molecule and scaffold to make graph of a molecule                
-                smiles = [id_to_smiles[k][0] for k in keys]    # list of whole SMILESs
-                scaffold = [id_to_smiles[k][1] for k in keys]  # list of scaffold SMILESs
+                # We need SMILESs of whole molecules and scaffolds to make molecule graphs.
+                wholes = [id_to_smiles[key][0] for key in keys]     # list of whole SMILESs
+                scaffolds = [id_to_smiles[key][0] for key in keys]  # list of scaffold SMILESs
 
-                p = mp.Process(target=train, args=(shared_model, shared_optimizer, smiles, scaffold, condition1, condition2, i, retval_list, args))
-                p.start()
-                processes.append(p)
+                proc = mp.Process(target=train, args=(shared_model, shared_optimizer, wholes, scaffolds, whole_conditions, scaffold_conditions, pid, retval_list, args))
+                proc.start()
+                processes.append(proc)
                 time.sleep(0.1)
-            for p in processes:
-                p.join() 
+            for proc in processes:
+                proc.join() 
             end = time.time()
 
             # retval_list shape -> (ncpus, item_per_cycle, 4),
@@ -192,7 +187,7 @@ shuffle_order     : {args.shuffle_order}
             loss1 = np.mean(np.array([losses[1] for k in retval_list for losses in k]))
             loss2 = np.mean(np.array([losses[2] for k in retval_list for losses in k]))
             loss3 = np.mean(np.array([losses[3] for k in retval_list for losses in k]))
-            print ('%s\t%s\t%s\t%.3f\t%.3f\t%.3f\t%.3f\t%.3f' %(epoch, c, epoch*num_cycles+c, loss, loss1, loss2, loss3, end-st))
-            if c%save_every==0:
-                name = args.save_dir+'/save_'+str(epoch)+'_' + str(c)+'.pt'
+            print ('%s\t%s\t%s\t%.3f\t%.3f\t%.3f\t%.3f\t%.3f' %(epoch, cycle, epoch*num_cycles+cycle, loss, loss1, loss2, loss3, end-st))
+            if cycle%save_every == 0:
+                name = args.save_dir+'/save_'+str(epoch)+'_' + str(cycle)+'.pt'
                 torch.save(shared_model.state_dict(), name)
