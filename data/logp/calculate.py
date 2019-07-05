8d7ad6ecaed898fd4729e3e85dafc9a453d93a37
mseok <mshmjp02@gmail.com>
2019-07-05 15:44:33 +0900

    data commit

--- data/logp/calculate.py
+++ data/logp/calculate.py
@@ -1,11 +1,22 @@
+# from rdkit import Chem
+# from rdkit.Chem.Crippen import MolLogP
+#
+# with open('../ChEMBL+STOCK1S/id_smiles_test.txt') as f, \
+#         open('../ChEMBL+STOCK1S/data_logp_test.txt', 'w') as w:
+#     for l in f:
+#         m_id, s1, s2 = l.split()
+#         m1, m2 = Chem.MolFromSmiles(s1), Chem.MolFromSmiles(s2)
+#         if m1 is None or m2 is None : continue
+#         c1,c2 = MolLogP(m1), MolLogP(m2)
+#         w.write(m_id+'\t'+str(c1)+'\t'+str(c2)+'\n')
 from rdkit import Chem
 from rdkit.Chem.Crippen import MolLogP
+with open("id_smiles_for_sample.txt") as oldFile, \
+    open("data_for_sample.txt", "w") as newFile:
+    for line in oldFile:
+        id, whole, scaffold = line.split("\t")
+        scaffold_mol = Chem.MolFromSmiles(scaffold)
+        if scaffold_mol is None: continue
+        condition_scaffold = MolLogP(scaffold_mol)
+        newFile.write(id + "\t" + "None" + "\t" + str(condition_scaffold) + "\n")
 
-with open('../ChEMBL+STOCK1S/id_smiles_test.txt') as f, \
-        open('../ChEMBL+STOCK1S/data_logp_test.txt', 'w') as w:
-    for l in f:
-        m_id, s1, s2 = l.split()
-        m1, m2 = Chem.MolFromSmiles(s1), Chem.MolFromSmiles(s2)
-        if m1 is None or m2 is None : continue
-        c1,c2 = MolLogP(m1), MolLogP(m2)
-        w.write(m_id+'\t'+str(c1)+'\t'+str(c2)+'\n')
